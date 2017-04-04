c998ad16b1599bb9cec9b563951701e5fabfcb36
Adam Thuvesen <a.thuvesen@gmail.com>
2017-04-04 15:51:09 +0200

    Navigation view updates

--- app/src/main/java/com/example/currentplacedetailsonmap/NavigationActivity.java
+++ app/src/main/java/com/example/currentplacedetailsonmap/NavigationActivity.java
@@ -1,13 +1,26 @@
 package com.example.currentplacedetailsonmap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class NavigationActivity extends AppCompatActivity {
+
+    // Side menu and toolbar customization.
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void stopSession(View view) {
+
     }
 }
